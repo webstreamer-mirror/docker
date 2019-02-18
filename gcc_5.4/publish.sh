446d0df509be51c9aa6409366355c925cfa25c27
Mingyi Zhang <mingyi.z@outlook.com>
2019-02-18 08:40:40 +0000

    .

--- gcc_5.4/publish.sh
+++ gcc_5.4/publish.sh
@@ -1,6 +1,6 @@
 imageID=$1
 tag=$2
 
-sudo docker login
-docker tag $imageID webstreamer/gcc54:$tag
-docker push webstreamer/gcc54:$tag
+sudo docker login  && \
+sudo docker tag $imageID webstreamer/gcc54:$tag && \
+sudo docker push webstreamer/gcc54:$tag
